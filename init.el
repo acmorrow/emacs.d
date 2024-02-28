4acd39c1a1c4ffb95a2ea9015c5ef82584c543e2
Andrew Morrow <andrew.morrow@viam.com>
2024-02-28 13:18:14 -0500

    keep customizations in a separate file

--- init.el
+++ init.el
@@ -3,3 +3,11 @@
 ;;
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
+
+
+;;
+;; Sideline custom.el
+;;
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
