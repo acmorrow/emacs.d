9f58e6cc8a9ed8d86cd5969d23f0e6b6ab516b8c
Andrew Morrow <andrew.morrow@viam.com>
2026-09-05 13:27:59 -0400

    Add lexical binding cookie to persisted custom.el

--- custom.el
+++ custom.el
@@ -1,9 +1,25 @@
+;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(package-selected-packages
+   '(ace-window anzu atomic-chrome auth-source-1password auto-compile
+		cape claude-code-ide consult consult-dir
+		consult-flycheck consult-flyspell consult-lsp
+		consult-projectile consult-yasnippet corfu crux
+		diff-hl diminish embark embark-consult ereader
+		flycheck flyspell-correct ghostel gptel guru-mode
+		lsp-mode lsp-ui magit marginalia no-littering
+		orderless protobuf-mode rg rust-mode rustic
+		solarized-theme undo-tree vertico volatile-highlights
+		yasnippet yasnippet-snippets))
+ '(package-vc-selected-packages
+   '((claude-code-ide :url
+		      "https://github.com/acmorrow/claude-code-ide.el")))
+ '(safe-local-variable-values
+   '((lsp-clients-clangd-args "--compile-commands-dir=build/local"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
